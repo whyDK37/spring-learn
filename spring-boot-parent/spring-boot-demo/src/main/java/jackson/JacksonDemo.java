89ef40e4a48425e03a009cab04fc9b95cbcc5f72
why <wanghuanyu10>
2020-06-11 10:44:04 +0800

    jackson demo

--- spring-boot-parent/spring-boot-demo/src/main/java/jackson/JacksonDemo.java
+++ spring-boot-parent/spring-boot-demo/src/main/java/jackson/JacksonDemo.java
@@ -2,6 +2,7 @@ package jackson;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonFactoryBuilder;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -24,7 +25,6 @@ public class JacksonDemo {
     jsonFactoryBuilder.enable(JsonFactory.Feature.CANONICALIZE_FIELD_NAMES);
     jsonFactoryBuilder.disable(JsonFactory.Feature.INTERN_FIELD_NAMES);
     jsonFactoryBuilder.configure(JsonWriteFeature.ESCAPE_NON_ASCII, true);
-
     //
     ObjectMapper objectMapper = new ObjectMapper(jsonFactoryBuilder.build());
 
@@ -33,13 +33,16 @@ public class JacksonDemo {
     System.out.println(
         "mapper.writeValueAsString(new User()) = " + objectMapper.writeValueAsString(user));
 
-    System.out.println("----------------------------------------------------------------------------------------------");
+    System.out.println("---------------------------------------------------------------------");
     // write enums index
     objectMapper.configure(SerializationFeature.WRITE_ENUMS_USING_INDEX, true);
     System.out.println("objectMapper.writeValueAsString(Color.BLUE) = " + objectMapper
         .writeValueAsString(Color.BLUE));
 
-    System.out.println("----------------------------------------------------------------------------------------------");
+    System.out.println("objectMapper.writeValueAsString(\"string value\") = " + objectMapper
+        .writeValueAsString("string value"));
+
+    System.out.println("---------------------------------------------------------------------");
     // 未知属性，不报错 properties
     // 方法1 设置 FAIL_ON_UNKNOWN_PROPERTIES属性
     // 方法2 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -48,7 +51,7 @@ public class JacksonDemo {
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     objectMapper.readValue(json, User.class);
 
-    System.out.println("----------------------------------------------------------------------------------------------");
+    System.out.println("---------------------------------------------------------------------");
     System.out.println("[ERROR]枚举反序列化，序列化的 city 使用 code， 反序列化使用的是 ordinal， 2.11.0 存在此bug");
     user.setWorkCities(Lists.newArrayList(CityEnum.BEIJING, CityEnum.HANGZHOU));
     System.out.println(objectMapper.writeValueAsString(user));
@@ -57,12 +60,18 @@ public class JacksonDemo {
     } catch (JsonProcessingException e) {
       System.out.println(e.getMessage());
     }
-    System.out.println("----------------------------------------------------------------------------------------------");
+    System.out.println("---------------------------------------------------------------------");
     System.out.println(
         "未知枚举值，在设置 @JsonEnumDefaultValue 之后，需要开启 READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE 配置");
     objectMapper.configure(READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
     json = "{\"workCities\":[9]}";
     System.out.println(objectMapper.readValue(json, User.class));
+
+    System.out.println("---------------------------------------------------------------------");
+    System.out.println("忽略 null 值");
+    objectMapper = new ObjectMapper();
+    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); //属性为NULL不序列化
+    System.out.println(objectMapper.writeValueAsString(new User().setName("why")));
   }
 
   enum Color {
