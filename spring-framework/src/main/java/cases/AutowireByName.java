8128abcbbb95dcc4af31620be361013ad8fa53c8
why <wanghuanyu10>
2020-06-16 09:29:11 +0800

    add doc

--- spring-framework/src/main/java/cases/AutowireByName.java
+++ spring-framework/src/main/java/cases/AutowireByName.java
@@ -5,13 +5,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 修改 xml 中或者 AutowiredConfig 文件中的 Primary，Service 会注入不同的 bean。
+ * <p>@Autowired 先匹配类型，如果有多个bean，选择优先级高的
+ * <p>@Resource 先匹配名称相同的 bean，如果没有则匹配类型，如果有多个类型相同的则匹配优先级高的。
+ *
  * @author wanghuanyu
  */
 public class AutowireByName {
 
   public static void main(String[] args) {
 
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+    ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
         "classpath:META-INF/cases/autowire-by-name.xml");
 
     Service service = applicationContext.getBean("service", Service.class);
--- spring-framework/src/main/java/cases/Service.java
+++ spring-framework/src/main/java/cases/Service.java
@@ -1,12 +1,13 @@
 package cases;
 
 import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Service {
 
   Store store;
 
-  @Resource
+  @Autowired
   public void setStore(Store store) {
     this.store = store;
   }
