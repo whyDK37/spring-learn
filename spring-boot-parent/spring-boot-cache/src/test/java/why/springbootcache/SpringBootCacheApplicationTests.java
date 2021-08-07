169b92ba21d46c4be2cf3419352bec305702e34d
why <why.DK37@gmail.com>
2021-08-07 21:38:03 +0800

    spring cache

--- spring-boot-parent/spring-boot-cache/src/test/java/why/springbootcache/SpringBootCacheApplicationTests.java
+++ spring-boot-parent/spring-boot-cache/src/test/java/why/springbootcache/SpringBootCacheApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
 
 @SpringBootTest(classes = {SpringBootCacheApplication.class})
 class SpringBootCacheApplicationTests {
@@ -11,11 +14,18 @@ class SpringBootCacheApplicationTests {
     @Autowired
     private FooService foo;
 
+    @Autowired
+    private CacheManager cacheManager;
+
     @Test
     void contextLoads() {
 
         String a = foo.echo("a");
         String echo = foo.echo("a");
         Assertions.assertTrue(a == echo);
+
+        Cache cache = cacheManager.getCache("cache");
+        System.out.println("cache.getClass() = " + cache.getClass());
+        CaffeineCache caffeineCache = (CaffeineCache) cache;
     }
 }
