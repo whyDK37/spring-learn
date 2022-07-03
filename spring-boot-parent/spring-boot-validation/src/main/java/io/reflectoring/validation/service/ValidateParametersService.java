221d1a665845d18ac87dc420e93d156bd4c44632
why <why.DK37@gmail.com>
2022-07-03 11:27:54 +0800

    增加日志收集

--- spring-boot-parent/spring-boot-validation/src/main/java/io/reflectoring/validation/service/ValidateParametersService.java
+++ spring-boot-parent/spring-boot-validation/src/main/java/io/reflectoring/validation/service/ValidateParametersService.java
@@ -3,14 +3,16 @@ package io.reflectoring.validation.service;
 import org.apache.skywalking.apm.toolkit.trace.Tag;
 import org.apache.skywalking.apm.toolkit.trace.Trace;
 import org.apache.skywalking.apm.toolkit.trace.TraceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ValidateParametersService {
 
-  @Trace(operationName = "返回默认值")
-  @Tag(key = "id", value = "${id}")
-  @Tag(key = "id2", value = "${id}")
+//  @Trace(operationName = "返回默认值")
+//  @Tag(key = "id", value = "${id}")
+//  @Tag(key = "id2", value = "${id}")
   public static String validatePathVariable(int id) {
     String traceId = TraceContext.traceId();
     System.out.println("traceId = " + traceId);
